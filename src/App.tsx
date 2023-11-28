50181b39191cd125bdba4727a92507ea5b719683
Markiian Kostiv <markiian.kostiv@gmail.com>
2023-11-28 21:17:45 +0200

    connect tailwind to the project

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export const App = () => {
   return (
-    <div>
-      <h1>my new project</h1>
+    <div className='bg-gray-500'>
+      <h1 className='text-red-500'>my new project</h1>
     </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import "./styles/tailwind.css";
+// import "./styles/tailwind.css";
 import { App } from "./App";
 
-import MyDick from "./mytscomponents/Mydick copy";
+// import MyDick from "./mytscomponents/Mydick copy";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
