0d545980431b101f1c8bc198eba0678cdfedecc8
Markiian Kostiv <markiian.kostiv@gmail.com>
2023-11-28 20:56:42 +0200

    claer project

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import "./styles/tailwind.css";
+import { App } from "./App";
+
+import MyDick from "./mytscomponents/Mydick copy";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(<App></App>);
