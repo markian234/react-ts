0c47f68d67623afc0568676ca19e6cb6a495851b
Markiian Kostiv <markiian.kostiv@gmail.com>
2023-11-29 22:48:58 +0200

    add craco

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 // import "./styles/tailwind.css";
-import { App } from "./App";
+import { App } from "@app/App";
 
 // import MyDick from "./mytscomponents/Mydick copy";
 
